2ffe9a546fc5b6b9da76521e563c9e5ac9dfa3b2
Lucasbc47 <lucasbarboza1costa@gmail.com>
2025-11-13 20:09:29 -0300

    feat(db): adiciona conexão com banco &amp; acrescenta connection factory

---
+++ src/main/java/br/gov/sp/cps/fatecipiranga/db/ConnectionFactory.java
@@ -0,0 +1,35 @@
+
+package br.gov.sp.cps.fatecipiranga.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactory {
+    private static String host = "pg-p2-fatecipiranga-projeto-p2-fatecipiranga.k.aivencloud.com";
+    private static String port = "24113";
+    private static String user = "avnadmin";
+    private static String password = "";
+    private static String database = "defaultdb";
+
+    static Connection getConnection() {
+        try {
+            String url = String.format(
+                    "jdbc:postgresql://%s:%s/%s",
+                    host, port, database);
+            Connection conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Connection conexao = getConnection();
+        if (conexao != null) {
+            System.out.println("Conectou!");
+        } else {
+            System.out.println("Não conectou!");
+        }
+    }
+}
