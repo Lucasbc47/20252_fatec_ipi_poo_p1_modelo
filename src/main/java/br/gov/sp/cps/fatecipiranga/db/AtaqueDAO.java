d4fe7c51a03bc9cdf7deb6a250a9060636d9afd3
Lucasbc47 <lucasbarboza1costa@gmail.com>
2025-11-26 18:36:02 -0300

    fix(ataqueDao): mantendo somente função cadastrar

--- src/main/java/br/gov/sp/cps/fatecipiranga/db/AtaqueDAO.java
+++ src/main/java/br/gov/sp/cps/fatecipiranga/db/AtaqueDAO.java
@@ -18,31 +18,6 @@ public class AtaqueDAO {
             ps.execute();
         }
     }
-    public void atualizar(Ataque a, int identificacao) throws Exception {
-        var sql = "UPDATE registros_de_ataques SET jogo=?,personagem=?,arma=?,vezes=?,mapa=? WHERE identificacao=?";
-        try (
-                var conexao = ConnectionFactory.getConnection();
-                var ps = conexao.prepareStatement(sql);) {
-            ps.setInt(1, a.getJogo());
-            ps.setString(2, a.getPersonagem());
-            ps.setString(3, a.getArma());
-            ps.setInt(4, a.getVezes());
-            ps.setString(5, a.getMapa());
-            ps.setInt(6, identificacao);
-
-            ps.execute();
-        }
-
-    }
-    public void apagar(int id) throws Exception {
-        var sql = "DELETE FROM registros_de_ataques WHERE identificacao=?";
-        try (
-                var conexao = ConnectionFactory.getConnection();
-                var ps = conexao.prepareStatement(sql);) {
-            ps.setInt(1, id);
-            ps.execute();
-        }
-    }
 }
 
 // Exemplo de registro:
