08d8acfffb5457257357f1ba595d48b270fd490b
Lucasbc47 <lucasbarboza1costa@gmail.com>
2025-11-25 21:59:18 -0300

    fix(tests): corrige tests

--- src/test/java/br/gov/sp/cps/fatecipiranga/PolicialTeste.java
+++ src/test/java/br/gov/sp/cps/fatecipiranga/PolicialTeste.java
@@ -1,27 +1,27 @@
-// package br.gov.sp.cps.fatecipiranga;
+package br.gov.sp.cps.fatecipiranga;
 
-// public class PolicialTeste {
-//     public static void main(String[] args) {
+public class PolicialTeste {
+    public static void main(String[] args) {
 
-//         // viabilizando testes com mapa
-//         String mapa = "Fatec Ipiranga";
-//         //Policial pessanha = new Policial("Peçanha", "fuzil", 3, 10);
-//         System.out.println(pessanha.getNomePolicial());
-//         System.out.println(pessanha.getArmamento());
-//         System.out.println(pessanha.getQuantidadeGranadas());
-//         System.out.println(pessanha.getEnergia());
+        // viabilizando testes com mapa
+        String mapa = "Fatec Ipiranga";
+        Policial pessanha = new Policial("Peçanha", 3, 10);
+        System.out.println(pessanha.getNomePolicial());
+        System.out.println(pessanha.getQuantidadeGranadas());
+        System.out.println(pessanha.getEnergia());
 
-//         //pessanha.atacar("fuzil", mapa);
-//         pessanha.lancarGranada(mapa);
-//         pessanha.desarmarBomba(mapa);
+        pessanha.atacar(mapa);
+        pessanha.lancarGranada(mapa);
+        pessanha.desarmarBomba(mapa);
+        pessanha.passarVez(mapa);
 
-//         System.out.println(pessanha.getEnergia());
-//         pessanha.receberDano(1);
-//         System.out.println(pessanha.getEnergia());
-//         pessanha.receberDano(2);
-//         System.out.println(pessanha.getEnergia());
-//         pessanha.receberDano(3);
-//         System.out.println(pessanha.getEnergia());
+        System.out.println(pessanha.getEnergia());
+        pessanha.receberDano(1);
+        System.out.println(pessanha.getEnergia());
+        pessanha.receberDano(2);
+        System.out.println(pessanha.getEnergia());
+        pessanha.receberDano(3);
+        System.out.println(pessanha.getEnergia());
 
-//     }
-// }
+    }
+}
--- src/test/java/br/gov/sp/cps/fatecipiranga/TerroristaTeste.java
+++ src/test/java/br/gov/sp/cps/fatecipiranga/TerroristaTeste.java
@@ -1,31 +1,30 @@
-// package br.gov.sp.cps.fatecipiranga;
+package br.gov.sp.cps.fatecipiranga;
 
-// public class TerroristaTeste {
-//     public static void main(String[] args) {
+public class TerroristaTeste {
+    public static void main(String[] args) {
 
-//         // viabilizando testes com mapa
-//         String mapa = "Roma";
-//         Terrorista terror = new Terrorista("mohammed", "fuzil", 3, 10);
+        // viabilizando testes com mapa
+        String mapa = "Roma";
+        Terrorista terror = new Terrorista("mohammed", 3, 10);
 
-//         // testando getters...
-//         System.out.println(terror.getNomeTerrorista());
-//         System.out.println(terror.getArmamento());
-//         System.out.println(terror.getQuantidadeGranadas());
-//         System.out.println(terror.getEnergia());
+        // testando getters...
+        System.out.println(terror.getNomeTerrorista());
+        System.out.println(terror.getQuantidadeGranadas());
+        System.out.println(terror.getEnergia());
 
-//         // metodos do terrorista
-//         terror.atacar("fuzil", mapa);
-//         terror.lancarGranada(mapa);
-//         terror.plantarBomba(mapa);
-//         terror.passarVez(mapa);
+        // metodos do terrorista
+        terror.atacar(mapa);
+        terror.lancarGranada(mapa);
+        terror.plantarBomba(mapa);
+        terror.passarVez(mapa);
 
-//         // dano & energia
-//         System.out.println(terror.getEnergia());
-//         terror.receberDano(1);
-//         terror.receberDano(2);
-//         terror.receberDano(3);
-//         terror.receberDano(50);
-//         System.out.println(terror.getEnergia());
+        // dano & energia
+        System.out.println(terror.getEnergia());
+        terror.receberDano(1);
+        terror.receberDano(2);
+        terror.receberDano(3);
+        terror.receberDano(50);
+        System.out.println(terror.getEnergia());
 
-//     }
-// }
+    }
+}
